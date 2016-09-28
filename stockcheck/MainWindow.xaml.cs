4f0f65d148d37d8d83de57ea6a3e09abe93a6d60
Andrew Bennet <andrewb@symplectic.co.uk>
2016-09-28 17:28:43 +0100

    Changed project from a Console app to a WPF app.

---
+++ stockcheck/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace stockcheck {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow {
+		public MainWindow() {
+			InitializeComponent();
+			WindowMessageLabel.Content = "Checking iPhone stock";
+		}
+
+		public DateTime LastStockCheck { get; private set; }
+		readonly StockChecker _stockChecker = new StockChecker();
+		private readonly int _sleepTime = int.Parse(ConfigurationManager.AppSettings["sleeptime"]);
+
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(_sleepTime) };
+			timer.Tick += (dispatchSender, args) => CheckStock();
+			timer.Start();
+		}
+
+		async void CheckStock()
+		{
+			string message = await _stockChecker.CheckForStockAsync();
+			LastStockCheck = DateTime.Now;
+			WindowMessageLabel.Content = $"Last stock check at {DateTime.Now.ToLongTimeString()}";
+			if (message != null)
+			{
+				MessageBox.Show(message, $"Stock found at {LastStockCheck}!", MessageBoxButton.OK, MessageBoxImage.Exclamation,
+					MessageBoxResult.OK, MessageBoxOptions.ServiceNotification);
+			}
+		}
+	}
+}
---
+++ stockcheck/StockChecker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace stockcheck
+{
+	public class StockChecker
+	{
+		public string Type { get; }
+		public string Colour { get; }
+		public string Model { get; }
+		public List<string> StoreNames { get; }
+		public Uri QueryUrl { get; }
+
+		public HttpClient HttpClient { get; } = new HttpClient();
+		public const string BaseUrl = "http://www.istocknow.com/live/live.php";
+
+		private List<string> _storeIds;
+
+		public StockChecker()
+		{
+			Type = ConfigurationManager.AppSettings["type"];
+			Colour = ConfigurationManager.AppSettings["colour"];
+			Model = ConfigurationManager.AppSettings["model"];
+			StoreNames = ConfigurationManager.AppSettings["stores"].Split(';').Select(str => str.Trim()).ToList();
+			QueryUrl = new Uri($"{BaseUrl}?type={Type}&color={Colour}&model={Model}&nocache={UnixTimestamp.Now}&ajax=1&operator=&nobb=false&notarget=false&noradioshack=false&nostock=false");
+		}
+
+
+		public async Task<string> CheckForStockAsync()
+		{
+			// Ping the API
+			HttpResponseMessage result = await HttpClient.GetAsync(QueryUrl);
+
+			// Load the response
+			string webContent = await result.Content.ReadAsStringAsync();
+			
+			JObject jObject;
+			if (JsonTools.TryParseJsonObject(webContent, out jObject))
+			{
+				JToken dataz = jObject["dataz"];
+				if (dataz != null)
+				{
+					// First time we query, we haven't cached the store IDs
+					if (_storeIds == null)
+					{
+						_storeIds = dataz.Values<JProperty>()
+							.Where(jProperty => StoreNames.Contains(jProperty.Value["title"].Value<string>()))
+							.Select(jProp => jProp.Name)
+							.ToList();
+					}
+
+					List<JProperty> stockedStores =
+						dataz.Values<JProperty>()
+							.Where(prop => _storeIds.Contains(prop.Name) && prop.Value["live"].Value<string>() != "0")
+							.ToList();
+
+					if (stockedStores.Any())
+					{
+						return $"Stock found at {string.Join(", ", stockedStores.Select(stockedStore => stockedStore.Value["title"].Value<string>()))}";
+					}
+				}
+			}
+			return null;
+		}
+	}
+
+	static class UnixTimestamp
+	{
+		public static long Now => (int) (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+	}
+
+	static class JsonTools
+	{
+		public static bool TryParseJsonObject(string value, out JObject jsonObject)
+		{
+			jsonObject = null;
+			try
+			{
+				using (JsonReader reader = new JsonTextReader(new StringReader(value)))
+				{
+					jsonObject = JObject.Load(reader);
+					return true;
+				}
+			}
+			catch (JsonReaderException)
+			{
+				return false;
+			}
+			catch
+			{
+				//yes, the JsonTextReader throws Exceptions, despite advertising JsonReaderException.
+				return false;
+			}
+		}
+	}
+}
