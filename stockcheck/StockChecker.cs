40eb68e6bf534d3d11cafaaa866422fcca88d944
Andrew Bennet <me@andrewbennet.com>
2016-09-29 08:19:44 +0100

    Changed app to query Apple site directly. Might not work.

--- stockcheck/StockChecker.cs
+++ stockcheck/StockChecker.cs
@@ -7,97 +7,79 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Text;
 
-namespace stockcheck
-{
-	public class StockChecker
-	{
-		public string Type { get; }
-		public string Colour { get; }
-		public string Model { get; }
-		public List<string> StoreNames { get; }
-		public Uri QueryUrl { get; }
+namespace stockcheck {
 
-		public HttpClient HttpClient { get; } = new HttpClient();
-		public const string BaseUrl = "http://www.istocknow.com/live/live.php";
+    public class StockChecker {
+        public List<IphoneModel> Models { get; }
+        public string PostCode { get; }
 
-		private List<string> _storeIds;
+        public HttpClient HttpClient { get; } = new HttpClient();
+        public const string BaseUrl = "http://www.apple.com/uk/shop/retail/pickup-message";//?parts.0=MN4U2B%2FA&location=E2+0DN&little=true";
 
-		public StockChecker()
-		{
-			Type = ConfigurationManager.AppSettings["type"];
-			Colour = ConfigurationManager.AppSettings["colour"];
-			Model = ConfigurationManager.AppSettings["model"];
-			StoreNames = ConfigurationManager.AppSettings["stores"].Split(';').Select(str => str.Trim()).ToList();
-			QueryUrl = new Uri($"{BaseUrl}?type={Type}&color={Colour}&model={Model}&nocache={UnixTimestamp.Now}&ajax=1&operator=&nobb=false&notarget=false&noradioshack=false&nostock=false");
-		}
+        public StockChecker() {
+            var IphoneSizes = ConfigurationManager.AppSettings["iphone-size"].Split(',').Select(typ => (IphoneSize)Enum.Parse(typeof(IphoneSize), typ.Trim())).ToList();
+            var Colours = ConfigurationManager.AppSettings["colour"].Split(',').Select(typ => (Colour)Enum.Parse(typeof(Colour), typ.Trim())).ToList();
+            var StorageSizes = ConfigurationManager.AppSettings["storage-size"].Split(',').Select(typ => (StorageSize)Enum.Parse(typeof(StorageSize), typ.Trim())).ToList();
+            Models = IphoneModel.GetModels(IphoneSizes, StorageSizes, Colours).ToList();
+
+            PostCode = ConfigurationManager.AppSettings["post-code"];
+        }
 
 
-		public async Task<string> CheckForStockAsync()
-		{
-			// Ping the API
-			HttpResponseMessage result = await HttpClient.GetAsync(QueryUrl);
+        public async Task<string> CheckForStockAsync() {
+            StringBuilder message = new StringBuilder();
 
-			// Load the response
-			string webContent = await result.Content.ReadAsStringAsync();
-			
-			JObject jObject;
-			if (JsonTools.TryParseJsonObject(webContent, out jObject))
-			{
-				JToken dataz = jObject["dataz"];
-				if (dataz != null)
-				{
-					// First time we query, we haven't cached the store IDs
-					if (_storeIds == null)
-					{
-						_storeIds = dataz.Values<JProperty>()
-							.Where(jProperty => StoreNames.Contains(jProperty.Value["title"].Value<string>()))
-							.Select(jProp => jProp.Name)
-							.ToList();
-					}
+            foreach(var iphoneModel in Models) {
+                var QueryUrl = new Uri($"{BaseUrl}?parts.0={iphoneModel.ToIdentifier()}&location={PostCode.Replace(' ', '+')}&little=false");
 
-					List<JProperty> stockedStores =
-						dataz.Values<JProperty>()
-							.Where(prop => _storeIds.Contains(prop.Name) && prop.Value["live"].Value<string>() != "0")
-							.ToList();
+                // Ping the API
+                HttpResponseMessage result = await HttpClient.GetAsync(QueryUrl);
 
-					if (stockedStores.Any())
-					{
-						return $"Stock found at {string.Join(", ", stockedStores.Select(stockedStore => stockedStore.Value["title"].Value<string>()))}";
-					}
-				}
-			}
-			return null;
-		}
-	}
+                // Load the response
+                string webContent = await result.Content.ReadAsStringAsync();
 
-	static class UnixTimestamp
-	{
-		public static long Now => (int) (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-	}
+                JObject jObject;
+                if(JsonTools.TryParseJsonObject(webContent, out jObject)) {
+                    JArray stores = jObject["body"]["stores"].Value<JArray>();
+                    if(stores != null) {
+                        foreach(var store in stores) {
+                            string pickupQuote = store["partsAvailability"][iphoneModel.ToIdentifier()]["storePickupQuote"].Value<string>();
+                            if(pickupQuote != "Currently unavailable") {
+                                message.AppendLine($"{iphoneModel.ToDisplayName()} available at {store["storeName"].Value<string>()}!");
+                            }
+                        }
+                    }
+                }
+            }
+            if(message.Length != 0) {
+                return message.ToString();
+            }
+            return null;
+        }
+    }
 
-	static class JsonTools
-	{
-		public static bool TryParseJsonObject(string value, out JObject jsonObject)
-		{
-			jsonObject = null;
-			try
-			{
-				using (JsonReader reader = new JsonTextReader(new StringReader(value)))
-				{
-					jsonObject = JObject.Load(reader);
-					return true;
-				}
-			}
-			catch (JsonReaderException)
-			{
-				return false;
-			}
-			catch
-			{
-				//yes, the JsonTextReader throws Exceptions, despite advertising JsonReaderException.
-				return false;
-			}
-		}
-	}
+    static class UnixTimestamp {
+        public static long Now => (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+
+    static class JsonTools {
+        public static bool TryParseJsonObject(string value, out JObject jsonObject) {
+            jsonObject = null;
+            try {
+                using(JsonReader reader = new JsonTextReader(new StringReader(value))) {
+                    jsonObject = JObject.Load(reader);
+                    return true;
+                }
+            }
+            catch(JsonReaderException) {
+                return false;
+            }
+            catch {
+                //yes, the JsonTextReader throws Exceptions, despite advertising JsonReaderException.
+                return false;
+            }
+        }
+    }
 }
---
+++ stockcheck/iPhoneModels.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace stockcheck {
+    public class IphoneModel {
+
+        public IphoneModel(IphoneSize iPhoneSize, StorageSize size, Colour colour) {
+            IphoneSize = iPhoneSize;
+            StorageSize = size;
+            Colour = colour;
+        }
+
+        public IphoneSize IphoneSize { get; }
+
+        public StorageSize StorageSize { get; }
+
+        public Colour Colour { get; }
+
+        public static string[] modelIdentifiers = new[] {
+            "?", "9C", "96", "8X", "92", "97", "8Y", "93", "98", "90", "94", "99", "9A", "91", "95",
+            "?", "4V", "51", "4M", "4W", "QM", "4P", "4X", "QN", "4Q", "4Y", "QP", "4U", "50", "QQ"
+        };
+
+        public string ToIdentifier() {
+            StringBuilder builder = new StringBuilder("MN");
+            int modelIndex = 15 * (int)IphoneSize + 3 * (int)Colour + (int)StorageSize;
+            return $"MN{modelIdentifiers[modelIndex]}2B/A";
+        }
+
+        public string ToDisplayName() {
+            StringBuilder displayName = new StringBuilder();
+            switch(IphoneSize) {
+                case IphoneSize.iphone7:
+                    displayName.Append("iPhone 7 ");
+                    break;
+                case IphoneSize.iphone7Plus:
+                    displayName.Append("iPhone 7 Plus ");
+                    break;
+            }
+            switch(Colour) {
+                case Colour.JetBlack:
+                    displayName.Append("Jet Black, ");
+                    break;
+                case Colour.Black:
+                    displayName.Append("Black, ");
+                    break;
+                case Colour.Silver:
+                    displayName.Append("Silver, ");
+                    break;
+                case Colour.Gold:
+                    displayName.Append("Gold, ");
+                    break;
+                case Colour.RoseGold:
+                    displayName.Append("Rose Gold, ");
+                    break;
+            }
+            switch(StorageSize) {
+                case StorageSize.GB32:
+                    displayName.Append("32GB");
+                    break;
+                case StorageSize.GB128:
+                    displayName.Append("128GB");
+                    break;
+                case StorageSize.GB256:
+                    displayName.Append("256GB");
+                    break;
+            }
+            return displayName.ToString();
+        }
+
+        public static IEnumerable<IphoneModel> GetModels(IEnumerable<IphoneSize> iphoneSizes, IEnumerable<StorageSize> sizes, IEnumerable<Colour> colours) {
+            foreach(var iphoneSize in iphoneSizes) {
+                foreach(var size in sizes) {
+                    foreach(var colour in colours) {
+                        if(colour != Colour.JetBlack || size != StorageSize.GB32) {
+                            yield return new IphoneModel(iphoneSize, size, colour);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public enum IphoneSize {
+        iphone7 = 0,
+        iphone7Plus = 1
+    }
+
+    public enum StorageSize {
+        GB32 = 0,
+        GB128 = 1,
+        GB256 = 2
+    }
+
+    public enum Colour {
+        JetBlack = 0,
+        Black = 1,
+        Silver = 2,
+        Gold = 3,
+        RoseGold = 4
+    }
+}
